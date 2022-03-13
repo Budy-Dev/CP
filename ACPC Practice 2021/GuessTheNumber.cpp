57244ab6936fc7d44c2a6f0beff7c2aad37dbf5f
Budy-Dev <63161425+Budy-Dev@users.noreply.github.com>
2022-03-13 21:55:00 +0100

    Add files via upload

---
+++ ACPC Practice 2021/GuessTheNumber.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	
+	long left = 1;
+	long right = 100000000;
+	int cont = 0;
+	
+	while (left <= right) {
+		cont++;
+		long N = (left + right) / 2;
+		
+		cout << N << endl;
+		cout.flush();
+		
+		string result;
+		cin >> result;
+		
+		if (result == "=") {
+			cout << "! " << N << endl << cont;
+			return 0;
+		} else if( result == "<"){
+			right = N - 1;
+		} else {
+			left = N + 1;
+		}
+	}
+	return 0;
+}
