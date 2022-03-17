327fe7fb46c073d535e0ff5257155f75b4bb96c3
Budy-Dev <63161425+Budy-Dev@users.noreply.github.com>
2022-03-17 15:35:24 +0100

    Add files via upload

---
+++ Kattis/Rijeci.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#define max 45
+
+using namespace std;
+
+
+int fib[45];
+
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	//freopen("rijeci.txt", "r", stdin);
+	
+	int n;
+	cin >> n;
+	
+	fib[0] = 0;
+	fib[1] = 1;
+	
+	if (n == 1) {
+		cout << fib[0] << " " << fib[1] << endl;
+		return 0;
+	}
+	
+	for (int i = 2; i < max; i++) {
+		fib[i] = fib[i-1] + fib[i-2];
+	}
+	
+	cout << fib[n-1] << " " << fib[n] << endl;
+	
+	return 0;
+}
